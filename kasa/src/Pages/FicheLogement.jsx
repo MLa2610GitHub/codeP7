2502d4279d6b5e199fa5927147f3538962c3b891
M La <mariela2610@gmail.com>
2022-11-02 16:55:36 +0100

    tests importations des photos

--- kasa/src/Pages/FicheLogement.jsx
+++ kasa/src/Pages/FicheLogement.jsx
@@ -7,20 +7,22 @@ import logements from '../logements.json';
 export default function  FicheLogement () {
     const params = useLocation();
     const fiche = logements.find(fiche => fiche.id === params?.state?.id);
-    
+       
+
     return (
       <>
-
-        <h1>{fiche?.title}</h1>
-        <p>{fiche.cover}</p>
-        <p>{fiche.pictures}</p>
-        <p>{fiche.description}</p>
-        <p>{fiche.host.name}</p>
-        <p>{fiche.host.picture}</p>
-        <p>{fiche.rating}</p>
-        <p>{fiche.location}</p>
-        <p>{fiche.equipments}</p>
-        <p>{fiche.tags}</p>
+        <div className="FicheLogement">
+          <img src={fiche.cover} alt="" className="coverPicture" />
+          <img src={fiche.pictures} alt="" />
+          <h1 className="titleFicheLogement">{fiche?.title}</h1>
+          <p className="tagFicheLogement">{fiche.tags}</p>
+          <p className="descriptionFicheLogement">{fiche.description}</p>
+          <p>{fiche.host.name}</p>
+          <img src={fiche.host.picture} alt="" className="hostPicture" />
+          <p>{fiche.rating}</p>
+          <p>{fiche.location}</p>
+          <p>{fiche.equipments}</p>
+        </div>
       </>
     );
 };
