9d4e91ce45d0e7a101142d188b291fd768d6cc35
M La <mariela2610@gmail.com>
2022-11-24 15:32:02 +0100

    slideshow flèches ok

--- kasa/src/Components/Slideshow.jsx
+++ kasa/src/Components/Slideshow.jsx
@@ -4,7 +4,7 @@ import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 import '../logements.json';
 import '../Styles/index.css';
 
-const Slideshow = ({ pictures }) => {
+export default function Slideshow({ pictures }) {
   const [currentSlide, setCurrentSlide] = useState(0);
 
   const next = () => {
@@ -24,33 +24,57 @@ const Slideshow = ({ pictures }) => {
     setCurrentSlide(current);
   };
 
-  return (
-    <>
-      <div className="container-slideshow">
-        <div className="slider">
-          {pictures.map((picture, index) => (
-            <div
-              className="slides-fade"
-              hidden={currentSlide === index ? false : true}
-            >
-              <div className="numbertext">
-                {index + 1} / {pictures.length}
+  if (pictures.length > 1) {
+    return (
+      <>
+        <div className="container-slideshow">
+          <div className="slider">
+            {pictures.map((picture, index) => (
+              <div
+                className="slides-fade"
+                hidden={currentSlide === index ? false : true}
+              >
+                <div className="numbertext">
+                  {index + 1} / {pictures.length}
+                </div>
+
+                <img src={picture} alt="" style={{ width: '100%' }} />
               </div>
+            ))}
 
-              <img src={picture} alt="" />
+            <div className="arrow-back" onClick={prev}>
+              <FaChevronLeft />
             </div>
-          ))}
-
-          <div className="arrow-back" onClick={prev}>
-            <FaChevronLeft />
-          </div>
-          <div className="arrow-forward" onClick={next}>
-            <FaChevronRight />
+            <div className="arrow-forward" onClick={next}>
+              <FaChevronRight />
+            </div>
           </div>
         </div>
-      </div>
-    </>
-  );
-};
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div className="container-slideshow">
+          <div className="slider">
+            {pictures.map((picture, index) => (
+              <div
+                className="slides-fade"
+                hidden={currentSlide === index ? false : true}
+              >
+                <div className="numbertext">
+                  {index + 1} / {pictures.length}
+                </div>
 
-export default Slideshow;
+                <img src={picture} alt="" style={{ width: '100%' }} />
+              </div>
+            ))}
+
+            <div className="arrow-back" FaChevronLeft={false}></div>
+            <div className="arrow-forward" FaChevronRight={false}></div>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
