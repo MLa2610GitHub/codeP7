a8c0246c21a4a553d1d2d1069088cf62f050a0f0
MLa2610GitHub <90907877+MLa2610GitHub@users.noreply.github.com>
2022-11-28 14:43:17 +0100

    Update FicheLogement.jsx
    intégration de l'id du logement dans l'url

--- kasa/src/Pages/FicheLogement.jsx
+++ kasa/src/Pages/FicheLogement.jsx
@@ -1,45 +1,50 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import Dropdown from '../Components/Dropdown';
 import Tags from '../Components/Tags';
 import logements from '../logements.json';
 import '../Styles/index.css';
 import Slideshow from '../Components/Slideshow';
 import Rating from '../Components/Rating';
+import ErrorPage from './ErrorPage';
 
-/*Le hook useLocation renvoie l'objet de localisation qui représente l'URL actuelle */
 /*Params permet de récupérer les paramètres passés dans l'URL */
 /*L'id permet de cibler les logements avec précision */
+/*La méthode find() renvoie la valeur du premier élément trouvé dans le tableau qui respecte la condition donnée par la fonction passée en argument. */
+/*Si l'id ne correspond pas à un logement, l'internaute est dirigé vers la page erreur */
 
 export default function FicheLogement() {
-  const params = useLocation();
-  const fiche = logements.find((fiche) => fiche.id === params?.state?.id);
+  const identifier = useParams();
+  const logement = logements.find((logement) => logement.id === identifier.id);
+  if (!logement) {
+    return <ErrorPage />;
+  }
 
   return (
     <>
       <main className="fiche-logement">
         <div className="carrousel-logement">
-          <Slideshow pictures={fiche.pictures} />
+          <Slideshow pictures={logement.pictures} />
         </div>
 
         <div className="data-logement">
           <div className="adress-logement">
             <div className="localisation-logement">
-              <h1 className="title-fiche-logement">{fiche?.title}</h1>
-              <p className="txt-location-logement">{fiche.location}</p>
+              <h1 className="title-fiche-logement">{logement?.title}</h1>
+              <p className="txt-location-logement">{logement.location}</p>
             </div>
 
             <div className="tagging-logement">
-              <Tags tags={fiche.tags} />
+              <Tags tags={logement.tags} />
             </div>
           </div>
 
           <div className="data-owner-logement">
             <div className="fiche-owner">
               <div className="details-owner">
-                <p>{fiche.host.name}</p>
+                <p>{logement.host.name}</p>
                 <img
-                  src={fiche.host.picture}
+                  src={logement.host.picture}
                   alt=""
                   className="fiche-logement-host-picture"
                 />
@@ -47,19 +52,22 @@ export default function FicheLogement() {
             </div>
 
             <div className="rating-logement">
-              <Rating rating={fiche.rating} />
+              <Rating rating={logement.rating} />
             </div>
           </div>
         </div>
 
         <div className="dropdown-logement">
           <div className="dropdown-item-content-description">
-            <Dropdown title={'Description'} description={fiche.description} />
+            <Dropdown
+              title={'Description'}
+              description={logement.description}
+            />
           </div>
           <div className="dropdown-item-content-equipments ">
             <Dropdown
               title={'Equipements'}
-              description={fiche.equipments.map((equipment) => (
+              description={logement.equipments.map((equipment) => (
                 <li key={equipment}> {equipment} </li>
               ))}
             />
